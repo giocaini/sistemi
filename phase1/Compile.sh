ed1bdab1654c1e55263bee10f044694f31bfcb0b
giocaini <gcaini95@gmail.com>
2017-02-07 10:37:19 +0100

    Compile.sh

--- phase1/Compile.sh
+++ phase1/Compile.sh
@@ -1,4 +1,3 @@
-
 arm-none-eabi-gcc -c -I /usr/include/uarm/ -mcpu=arm7tdmi p1test.c -o p1test.o
 arm-none-eabi-gcc -c -I /usr/include/uarm/ -mcpu=arm7tdmi mikabooq.c -o mikabooq.o
 arm-none-eabi-ld -T /usr/include/uarm/ldscripts/elf32ltsarm.h.uarmcore.x -o p1test /usr/include/uarm/crtso.o /usr/include/uarm/libuarm.o p1test.o mikabooq.o
